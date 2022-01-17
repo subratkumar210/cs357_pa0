bb91ba7ed72f016a96fa20210befd88d50fe1db3
subratkumar210 <66162164+subratkumar210@users.noreply.github.com>
2022-01-17 18:41:24 +0530

    Update encry.py

--- encry.py
+++ encry.py
@@ -1,8 +1,10 @@
+# python module to create user interface
 from tkinter import *
 en = Tk()
 en.title("CSE-357__PA0/19165055")
 en.geometry("400x400")
 
+#function for ciphering and deciphering
 def change(s):
     itable={ 
         'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s','i':'r','j':'q','k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'g','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':' ','A':'Z','B':'Y','C':'X','D':'W','E':'V','F':'U','G':'T','H':'S','I':'R','J':'Q','K':'P','L':'O','M':'N','N':'M','O':'L','P':'K','Q':'J','R':'I','S':'H','T':'G','U':'F','V':'E','W':'D','X':'C','Y':'B','Z':'A'
@@ -13,15 +15,18 @@ def change(s):
     
     return ans	
 
+#function for encryption
 def for_e():					
 	et = change( e1.get())
 	e2.insert(0, et)
 
+#function for decryption 
 def for_p():					
 	pt = change(e3.get())
 	e4.insert(0, pt)
 	
 
+#creation of user interface
 l1 = Label(en, text ="Plain text")			
 l1.grid(row = 10, column = 1)
 l2 = Label(en, text ="Cipher text")
